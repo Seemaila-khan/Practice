4b1e735764b6b6427960060a704e4f5837d9dca2
Seema <Seema@LAPTOP-1SM5FAE7>
2020-12-17 18:25:22 +0530

    2nd commit

--- DemoApp2/src/com/demo/Sample.java
+++ DemoApp2/src/com/demo/Sample.java
@@ -3,7 +3,8 @@ package com.demo;
 public class Sample {
 
 	public static void main(String[] args) {
-     System.out.println("hii");
+    System.out.println("hii");
+    System.out.println("hii again");
 
 	}
 
